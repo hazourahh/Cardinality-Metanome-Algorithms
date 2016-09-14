d0f99b705200aad7e9105cda2aaa5a6546eb265d
hazourahh <hazourahh@gmail.com>
2016-09-14 14:37:09 +0200

    modify superloglog to make it use the same alpha as loglog

--- DVSuperLogLog/src/main/java/de/metanome/algorithms/dvsuperloglog/SuperLogLog.java
+++ DVSuperLogLog/src/main/java/de/metanome/algorithms/dvsuperloglog/SuperLogLog.java
@@ -44,9 +44,42 @@ public class SuperLogLog {
   /**
    * Correction function
    */
+  protected static final double[] mAlpha = {
+      0,
+      0.44567926005415,
+      1.2480639342271,
+      2.8391255240079,
+      6.0165231584809,
+      12.369319965552,
+      25.073991603111,
+      50.482891762408,
+      101.30047482584,
+      202.93553338100,
+      406.20559696699,
+      812.74569744189,
+      1625.8258850594,
+      3251.9862536323,
+      6504.3069874480,
+      13008.948453415,
+      26018.231384516,
+      52036.797246302,
+      104073.92896967,
+      208148.19241629,
+      416296.71930949,
+      832593.77309585,
+      1665187.8806686,
+      3330376.0958140,
+      6660752.5261049,
+      13321505.386687,
+      26643011.107850,
+      53286022.550177,
+      106572045.43483,
+      213144091.20414,
+      426288182.74275,
+      852576365.81999
+};
+
     private double Ca;
-    private final double ca1=0.79402;
-    private final double ca2=0.84249;
   /**
    * The generated hash functions
    */
@@ -61,8 +94,7 @@ public class SuperLogLog {
        this.M = new byte[numofbucket];
        //truncation rule parameters
        this.truncatednumofbucket=(int)Math.floor(0.7*numofbucket);
-       this.Ca = ca1* truncatednumofbucket-ca2;
-       
+       this.Ca = mAlpha[Numbits]/numofbucket;
        //restriction Rule parameters
        B=Math.ceil(Math.log(Nmax/numofbucket)/Math.log(2)+3);
   }
@@ -96,6 +128,6 @@ public class SuperLogLog {
        if(M[j]>0 && M[j]<B)
         Rsum+=M[j];// the trancated sum
     double Ravg = Rsum / (double) truncatednumofbucket;
-    return (long) Math.floor(Ca* Math.pow(2, Ravg));
+    return (long) Math.floor(Ca*truncatednumofbucket* Math.pow(2, Ravg));
 }
   }
